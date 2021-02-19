d56845dd917cca57e11a1c1a26848a8e083e8aa2
yi.qrcode <yi.qrcode@gmail.com>
2021-02-20 04:03:45 +0900

    20210220 cKnow 라우터 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
-var router = express.Router();
+const mysql   = require('mysql');
+const dbconfig = require('../bin/database.js');
+
+const conn = mysql.createConnection(dbconfig);
+
+var app = express();
+//var router = express.Router();
+
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
+module.exports = app;
